bedde40787cba147c6f1939776705a55b5e0a234
tanverrana <tanversobur@gmail.com>
2022-05-06 17:28:47 +0600

    show all data

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -15,7 +16,17 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.evbbw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
-    try { }
+    try {
+        await client.connect();
+        const productCollection = client.db("emaJohn").collection("product");
+
+        app.get("/product", async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+    }
     finally { }
 }
 
