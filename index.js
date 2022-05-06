a57d42cd8261e8d4fe697b8810a1acc763bc2f92
tanverrana <tanversobur@gmail.com>
2022-05-06 15:21:54 +0600

    connect mongo

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -7,6 +8,18 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.evbbw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try { }
+    finally { }
+}
+
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
     res.send("john is running and ema is requesting")
